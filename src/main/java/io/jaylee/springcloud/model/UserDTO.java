f800776fe99e6574f1882502e6f19d93b364679e
Jay Lee <eggboy@gmail.com>
2023-02-27 11:58:30 +0800

    fix: Changed UserDTO Record with POJO

--- src/main/java/io/jaylee/springcloud/model/UserDTO.java
+++ src/main/java/io/jaylee/springcloud/model/UserDTO.java
@@ -1,6 +1,18 @@
 package io.jaylee.springcloud.model;
 
-public record UserDTO(String userId, String lastName, String firstName, String email) {
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@ToString
+public class UserDTO {
+
+    String userId;
+    String lastName;
+    String firstName;
+    String email;
 }
 
 
